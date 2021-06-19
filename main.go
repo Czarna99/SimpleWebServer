44a9eb309993f2507f63039243c37f161f342c7f
Paweł Joachimczuk <pawel@MacBook-Pro-Pawe.local>
2021-06-19 11:40:40 +0200

    removed unnesesary lines from main file

--- main.go
+++ main.go
@@ -9,9 +9,5 @@ var Attraction []data.Attraction
 
 func main() {
 	app.HandleRequests()
-	Attraction = []data.Attraction{
-		data.Attraction{Id: 1, Name: "Eiffel Tower", City: "Paris", Country: "France"},
-		data.Attraction{Id: 2, Name: "Tower", City: "London", Country: "Great Britan"},
-	}
 
 }
